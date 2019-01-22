b6344acd18c9c358903c5c89c145aec56247a34d
Alonso Ortega <alonsojortega@gmail.com>
2019-01-22 07:47:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz
-
+  if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
+      "Fizzbuzz" # Go fizz
+  elsif int % 3 == 0
+      "Fizz"
+  elsif int % 5 == 0
+      "Buzz"
+  else
+      nil
+  end
 end
